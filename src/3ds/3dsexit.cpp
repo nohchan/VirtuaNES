6b99982677993149346b6fc87626ffecfa9168ee
nohchan <nohchan@gantan.net>
2026-09-25 20:41:08 +0900

    Update 3dsexit.cpp

--- src/3ds/3dsexit.cpp
+++ src/3ds/3dsexit.cpp
@@ -1,7 +1,15 @@
-// 一番上の行に追記
+// 1. 標準ライブラリと競合するマクロを一度完全に消去する
 #undef _NOTHROW
+#undef __alloc_size
+#undef __alloc_size2
+
+// 2. この安全な状態で標準ライブラリをすべて読み込ませる
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+// 3. 読み込みが終わったら、エミュレータ側の元の仕様に戻す
+#define _NOTHROW
 
 #include <3ds.h>
 
