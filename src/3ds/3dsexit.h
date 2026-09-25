c593ead7b1eb5b479b450f912a6cc9ea5094df5a
nohchan <nohchan@gantan.net>
2026-09-25 20:44:59 +0900

    Update 3dsexit.h

--- src/3ds/3dsexit.h
+++ src/3ds/3dsexit.h
@@ -1,6 +1,16 @@
 #ifndef _3DSEXIT_H_
 #define _3DSEXIT_H_
 
+// 一番上の行から追記して、標準ライブラリの読み込み時だけマクロを消滅させる
+#undef _NOTHROW
+#undef __alloc_size
+#undef __alloc_size2
+
+#include <stdlib.h>
+
+// 読み込みが終わったら、エミュレータ側の元の仕様に戻す
+#define _NOTHROW
+
 #include <3ds.h>
 
 extern aptHookCookie hookCookie;
